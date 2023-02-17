b3fd9e042676d4e0af0edac4ddb40b5f5f8271f8
yndz <ayandamthembu031@gmail.com>
2023-02-17 06:14:36 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)/*prints tens digit*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)/*prints ones digit*/
+		{
+			if (!((ones == tens) || (tens > ones)))/*eliminates repetition*/
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/*added comma and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
