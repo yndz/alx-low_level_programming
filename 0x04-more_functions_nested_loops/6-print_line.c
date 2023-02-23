6b17a7e253c6109a6ae50a1d1f17c918bc6c458b
yndz <ayandamthembu031@gmail.com>
2023-02-23 03:15:07 -0800

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line acc to param
+ * @n: The number of lines to be drawn
+ * Return: empty
+ */
+
+void print_line(int n)
+
+{
+	int x;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; x++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
