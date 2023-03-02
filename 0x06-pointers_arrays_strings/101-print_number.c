1afa22e4d400066b65116815dd561cd39723a143
yndz <ayandamthembu031@gmail.com>
2023-03-02 12:30:38 -0800

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_number - print numbers chars
  * @n: integer params
--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,1 +1,15 @@
-typedef int make_iso_compilers_happy;
+#include "main.h"                                                                                                                  
+#include <unistd.h>                                                                                                                     
+                                                                                                                                        
+/**                                                                                                                                     
+ * _putchar - writes the character c to stdout                                                                                          
+ * @c: The character to print                                                                                                           
+ *                                                                                                                                      
+ * Return: On success 1.                                                                                                                
+ * On error, -1 is returned, and errno is set appropriately.                                                                            
+ */                                                                                                                                     
+int _putchar(char c)                                                                                                                    
+{                                                                                                                                       
+        return (write(1, &c, 1));                                                                                                       
+}
+
